76495dbd0eb25480cb3e2c061e36f5c33ecc5aa9
ASHUTOSH SWAIN <ashutoshswain465@gmail.com>
2024-12-06 15:11:29 +0530

    Creating header, pdf and Readme

---
+++ README.md
@@ -0,0 +1,2 @@
+# What is this project?
+This project is to read the text files and generate single PDF file
--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-import pandas as pd
 import glob
 from fpdf import FPDF
 from pathlib import Path
 
-filepaths =glob.glob("Text_files/*.txt")
+filepaths = glob.glob("Text_files/*.txt")
+
+pdf = FPDF(orientation="P", unit="mm", format="A4")
+pdf.set_auto_page_break(auto=False, margin=0)
 
 for filepath in filepaths:
-    df = pd.re
+    pdf.add_page()
+    page_name = Path(filepath).stem.title()
+    pdf.set_font(family="Times", size=16, style="B")
+    pdf.cell(w=50, h=18, txt=f"{page_name}")
+
+pdf.output("output.pdf")
